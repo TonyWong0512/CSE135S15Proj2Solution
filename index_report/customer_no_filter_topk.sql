43e06cb8cc7f16758a529e00034479ce029b6ef8
cse135 <cse135@ucsd.edu>
2015-05-01 01:14:09 -0700

    more

--- index_report/customer_no_filter_topk.sql
+++ index_report/customer_no_filter_topk.sql
@@ -2,9 +2,9 @@ BEGIN;
 CREATE TEMP TABLE p_t (name text, id int, price int) ON COMMIT DELETE ROWS;
 CREATE TEMP TABLE u_t (name text, id int, price int) ON COMMIT DELETE ROWS;
 EXPLAIN ANALYZE VERBOSE insert into u_t (name, id, price) select u.name, s.uid, sum(s.quantity*s.price) as price
-from  users u, sales s  where s.uid=u.id group by s.uid, u.name order by price desc offset 0 limit 20;
+from  users u left outer join sales s on s.uid=u.id group by s.uid, u.name order by price desc offset 0 limit 20;
 EXPLAIN ANALYZE VERBOSE insert into p_t (name, id, price) select p.name, s.pid, sum(s.quantity*s.price) as price 
-from products p, sales s where s.pid=p.id  group by s.pid, p.name order by price desc offset 0 limit 10;
+from products p left outer join sales s on s.pid=p.id  group by s.pid, p.name order by price desc offset 0 limit 10;
 -- select * from u_t;
 -- select * from p_t;
 EXPLAIN ANALYZE select s.uid, s.pid, sum(s.quantity*s.price) from u_t u,p_t p, sales s where s.uid=u.id and s.pid=p.id group by s.uid, s.pid;
