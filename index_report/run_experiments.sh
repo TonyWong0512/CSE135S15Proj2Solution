bc947cdd4a4b2c7c04bc6b6c59a7bc76d2f043f1
cse135 <cse135@ucsd.edu>
2015-05-04 21:27:06 -0700

    adding missing files to index report

--- index_report/run_experiments.sh
+++ index_report/run_experiments.sh
@@ -7,25 +7,25 @@ sudo service postgresql start
 psql -f customer_no_filters.sql -d cse135_big > cold_4.5GB/customer_no_filters
 
 ## search by customer filter by category order by name
-psql -f customer_filter_category.sql -d cse135_small > hot_10MB/customer_no_filters
-psql -f customer_filter_category.sql -d cse135_small > hot_10MB/customer_no_filters
+psql -f customer_filter_category.sql -d cse135_small > hot_10MB/customer_filter_category
+psql -f customer_filter_category.sql -d cse135_small > hot_10MB/customer_filter_category
 sudo service postgresql stop
 sudo sh -c "echo 3 > /proc/sys/vm/drop_caches"
 sudo service postgresql start
-psql -f customer_filter_category.sql -d cse135_big > cold_4.5GB/customer_no_filters
+psql -f customer_filter_category.sql -d cse135_big > cold_4.5GB/customer_filter_category
 
 ## search by customer no filters order by price
-psql -f customer_no_filter_topk.sql -d cse135_small > hot_10MB/customer_no_filters
-psql -f customer_no_filter_topk.sql -d cse135_small > hot_10MB/customer_no_filters
+psql -f customer_no_filter_topk.sql -d cse135_small > hot_10MB/customer_no_filter_topk
+psql -f customer_no_filter_topk.sql -d cse135_small > hot_10MB/customer_no_filter_topk
 sudo service postgresql stop
 sudo sh -c "echo 3 > /proc/sys/vm/drop_caches"
 sudo service postgresql start
-psql -f customer_no_filter_topk.sql -d cse135_big > cold_4.5GB/customer_no_filters
+psql -f customer_no_filter_topk.sql -d cse135_big > cold_4.5GB/customer_no_filter_topk
 
 ## search by state no filters order by topk
-psql -f states_no_filter_topk.sql -d cse135_small > hot_10MB/customer_no_filters
-psql -f states_no_filter_topk.sql -d cse135_small > hot_10MB/customer_no_filters
+psql -f states_no_filter_topk.sql -d cse135_small > hot_10MB/states_no_filter_topk
+psql -f states_no_filter_topk.sql -d cse135_small > hot_10MB/states_no_filter_topk
 sudo service postgresql stop
 sudo sh -c "echo 3 > /proc/sys/vm/drop_caches"
 sudo service postgresql start
-psql -f states_no_filter_topk.sql -d cse135_big > cold_4.5GB/customer_no_filters
+psql -f states_no_filter_topk.sql -d cse135_big > cold_4.5GB/states_no_filter_topk
